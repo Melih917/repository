6a3a6e10e5c0d072af6d6eec9ee762a20dee5bde
Melih917 <78846958+Melih917@users.noreply.github.com>
2024-01-15 03:26:01 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,93 @@
+const prevBtn = document.querySelector("#prev-btn");
+const nextBtn = document.querySelector("#next-btn");
+const book = document.querySelector("#book");
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
+
+
+const paper1 = document.querySelector("#p1");
+const paper2 = document.querySelector("#p2");
+const paper3 = document.querySelector("#p3");
+
+
+
+
+const papers = [
+    document.querySelector("#p1"),
+    document.querySelector("#p2"),
+    document.querySelector("#p3"),
+];
+
+
+prevBtn.addEventListener("click", goPrevPage);
+nextBtn.addEventListener("click", goNextPage);
+darkModeToggle.addEventListener("click", toggleDarkMode);
+
+let currentLocation = 1;
+let numOfPapers = 3;
+let maxLocation = numOfPapers + 1;
+
+function openBook() {
+    book.style.transform = "translateX(50%)";
+    prevBtn.style.transform = "translateX(-180px)";
+    nextBtn.style.transform = "translateX(180px)";
+}
+
+function closeBook(isAtBeginning) {
+    if(isAtBeginning) {
+        book.style.transform = "translateX(0%)";
+    } else {
+        book.style.transform = "translateX(100%)";
+    }
+    
+    prevBtn.style.transform = "translateX(0px)";
+    nextBtn.style.transform = "translateX(0px)";
+}
+
+function goNextPage() {
+    if (currentLocation < maxLocation) {
+        const currentPaper = papers[currentLocation - 1];
+
+        openBook();
+
+        currentPaper.classList.add("flipped");
+        currentPaper.style.zIndex = currentLocation;
+
+        if (currentLocation === maxLocation - 1) {
+            closeBook();
+        }
+
+        currentLocation++;
+    }
+}
+
+function goPrevPage() {
+    if(currentLocation > 1) {
+        switch(currentLocation) {
+            case 2:
+                closeBook(true);
+                paper1.classList.remove("flipped");
+                paper1.style.zIndex = 3;
+                break;
+            case 3:
+                paper2.classList.remove("flipped");
+                paper2.style.zIndex = 2;
+                break;
+            case 4:
+                openBook();
+                paper3.classList.remove("flipped");
+                paper3.style.zIndex = 1;
+                break;
+            default:
+                throw new Error("unkown state");
+        }
+
+        currentLocation--;
+    }
+}
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+    const body = document.body;
+
+
+}
